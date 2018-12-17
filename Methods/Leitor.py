30e1d2efdf90265ef034cfa17c7e0273b1faa375
Rodrigo Prado <rodrigo11spbr@outlook.com>
2018-12-17 17:14:08 -0200

    Adição da biblioteca numpy para criação da matriz onde sera adicionado os valores de luminosidade

--- Methods/Leitor.py
+++ Methods/Leitor.py
@@ -1,15 +1,28 @@
 from PIL import Image
+import numpy as np
 
 caminhoImagem = 'AnaliseImagem/Imgs/Mario.jpg'
-def getMatriz(caminhoImagem):
-    with Image.open(caminhoImagem) as i :
-        pixels = i.getpixel((0, 0))
-        dimensoes = width, height = i.size
 
-        print('altura {}'.format(height))
-        print('largura {}'.format(width))
-        print('pixels {}'.format(pixels))
+def getMatrizLuminosidade(caminhoImagem) :
+    
+        imagemRenderizada = Image.open(caminhoImagem).convert('RGB')
+
+        width, height = imagemRenderizada.size
+
+        for x in range(width) :
+                for y in range(height) :        
+                        r, g, b = imagemRenderizada.getpixel((x, y))
+
+                        # Calculo luminosidade
+                        red = round(r * 0.3)
+                        green = round(g * 0.59)
+                        blue = round(b * 0.11)
+                        
+                        imagemMatriz = np.matrix('{} {} {}'.format(red, green, blue))
+
+                        # imagemMatriz = [[red, green, blue]]
+
+        print(imagemMatriz)
         
-
 if __name__ == '__main__' :
-    getMatriz(caminhoImagem)
+    getMatrizLuminosidade(caminhoImagem)
