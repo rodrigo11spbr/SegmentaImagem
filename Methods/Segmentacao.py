12dfcbd9c0fa9668ff3f69832b44dfc2a4f345a1
Rodrigo Prado <rodrigo11spbr@outlook.com>
2018-12-26 11:13:41 -0200

    troca de imagem por uma menor

--- Methods/Segmentacao.py
+++ Methods/Segmentacao.py
@@ -1,34 +1,29 @@
 from processamento import Processamento
 
-caminhoImagem = 'AnaliseImagem/Imgs/teste.jpg'
-CONST_MEDIA = 20
+caminhoImagem = 'AnaliseImagem/Imgs/Mario.jpg'
+CONST_MEDIA = 100
 
 def segmentar(caminhoImagem) :
 
         largura, altura, imagem = Processamento.get_propriedade_imagem(caminhoImagem)
-        matriz = Processamento.get_matriz_mae(largura, altura, imagem)
+        matriz_original = Processamento.get_matriz_mae(largura, altura, imagem)
         
-        MatrizSegmentada = [[0 for x in range(altura)] for y in range(largura)]
+        matriz_segmentada = [[0 for x in range(altura)] for y in range(largura)]
 
         for x in range(0, altura) :
                 for y in range(0, largura) :
 
-                        # FUNÇÃO DE MUNFORD–SHAH A SER MELHORADA
-                        constante_pixel = matriz[x][y]
+                        # FUNÇÃO DE MUNFORD–SHAH
+                        constante_pixel = matriz_original[y][x]     
                         for a in range(0, altura) :
-                                for b in range(0, largura) :
-                                        pixel = matriz[b][a]
+                                for l in range(0, largura) :
+                                        pixel = matriz_original[l][a]
                                         if Processamento.verifica_pixel(pixel, constante_pixel, CONST_MEDIA) :
-                                                MatrizSegmentada[b][a] = pixel
+                                                matriz_segmentada[l][a] = pixel
                                         else :
-                                                MatrizSegmentada[b][a] = 0
+                                                matriz_segmentada[l][a] = 0
 
-                        # if matriz[y][x] != 185 and matriz[y][x] != 186 :
-                        #         MatrizSegmentada[y][x] = matriz[y][x]
-                        # else :
-                        #         MatrizSegmentada[y][x] = 0
-
-        Processamento.montar_imagem_array(MatrizSegmentada)
+        Processamento.montar_imagem_array(matriz_segmentada)
 
 if __name__ == '__main__' :
         segmentar(caminhoImagem)
